cdaa3602cced12202da125362a7479129b83473a
Sidhant <sidhantrc02@gmail.com>
2021-03-29 13:33:03 +0530

    done

---
+++ fb-backend/mongoPosts.js
@@ -0,0 +1,14 @@
+// mongo db schemaa
+
+import mongoose from 'mongoose'
+
+const postModel = mongoose.Schema({
+    user: String,
+    imgName: String ,
+    text : String,
+    avatar : String ,
+    timestamp : String
+})
+export default mongoose.model('posts', postModel)
+
+// posts is the collection where you will bw saving your post
